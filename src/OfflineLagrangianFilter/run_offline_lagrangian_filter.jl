0f352bd2b343797f86af89a671a12fd979b298fb
julie <juliemnier@gmail.com>
2026-08-25 17:42:53 +0100

    save Xi without regridding for non-rectilinear grids

--- src/OfflineLagrangianFilter/run_offline_lagrangian_filter.jl
+++ src/OfflineLagrangianFilter/run_offline_lagrangian_filter.jl
@@ -115,8 +115,13 @@ function run_offline_Lagrangian_filter(config)
     end
 
     # Option to regrid to mean position
-    if config.map_to_mean
+    # Add the option to save displacement without regridding for non rectilinear grids
+    underlying = config.grid isa ImmersedBoundaryGrid ? config.grid.underlying_grid : config.grid
+    if config.map_to_mean && underlying isa RectilinearGrid
         regrid_to_mean_position!(config)
+    elseif config.map_to_mean
+        @info "Non-rectilinear grid ($(nameof(typeof(underlying)))): ξ maps written to " *
+              "$(config.output_filename); final interpolation to mean position skipped."
     end
 
     # Option to calculate Eulerian filter too
